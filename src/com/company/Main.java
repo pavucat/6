5d1471e751152628090d4dd6b3d73baeee7f6939
FoPQer <iritskiy.sas@gmail.com>
2020-09-26 14:44:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Movable one = new Movablerectangle(0, 1, 0, 1, 0, 0, 10, 20, "red", true);
+	System.out.println(one.toString());
+    }
+}
---
+++ src/com/company/Movable.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Movable {
+    void moveup();
+    void movedown();
+    void left();
+    void right();
+}
